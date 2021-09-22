36ed5e793f2ba04f8f8579ea578bb0bc866d2a4b
Jay <jhooey007@gmail.com>
2021-09-22 12:32:40 -0500

    added alerts

--- main.js
+++ main.js
@@ -7,6 +7,24 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "0123456789"
 var symbols = "!@#$%^&*()_+"
 
+var genPasswordButton = document.querySelector('#generateButton');
+var copyPasswordButton = document.querySelector('#copyButton');
+
+genPasswordButton.addEventListener('click', function() {
+    passReset();
+    passLength = prompt('Choose a password length between 8 and 128 characters.');
+    if (parseInt(passLength) < 8 || parseInt(passLength) > 128) {
+        alert('Invalid length. Please choose a length between 8 and 128 characters.');
+    } else if (passLength === null) {
+        alert('A password will not be generated.');
+    } else if (isNaN(passLength) === true || passLength === '') {
+        alert('Please use numerical numbers and try again.');
+    } else {
+        selectedCharacters();
+    }
+    document.getElementById('password-form').textContent = userPassword;
+});
+
 function writePassword() {
  
 
